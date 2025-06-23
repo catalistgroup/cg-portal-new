89e432b2e40612c250f9eaa6bf089fe0c3697694
Simranjit Singh <simranjit.singh@calidig.com>
2025-06-23 17:51:24 +0530

    fix: only admin user can go to admin-inventory

--- apps/client/app/(store)/store/[storeId]/admin-inventory/page.tsx
+++ apps/client/app/(store)/store/[storeId]/admin-inventory/page.tsx
@@ -3,6 +3,7 @@ import { getCookie } from 'cookies-next';
 import { cookies, headers } from 'next/headers';
 import { parseUser } from '@/utils/helper-function';
 import { ModifyInventory } from '@/components/product-analysis/modify-inventory';
+import { redirect } from 'next/navigation';
 
 type Props = {
   params: Promise<{ storeId: string }>;
@@ -35,6 +36,10 @@ async function ProductAnalysisPage({ params }: Props) {
   const user = parseUser(String(userString));
 
   const isAdmin = user?.is_superuser;
+
+  if (!isAdmin) {
+    return redirect("/store");
+  }
 
   return (
     <div className="p-4">
